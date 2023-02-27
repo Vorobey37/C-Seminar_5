e32ecacb74ec6be7710f0090e90e501ddbe53169
Abvorobey <abvorobey@gmail.com>
2023-02-27 15:08:33 +0300

    Метод с bool, split, join, а также перевод текста в массив

---
+++ Number_In_Mass/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Задайте массив.Напишите программу, 
+которая определяет, присутствует ли заданное число в массиве.
+
+4; массив[6, 7, 19, 345, 3]->нет
+
+*/
+Console.Clear();
+Console.WriteLine ("Введите элементы массива:");
+string text  = Console.ReadLine();
+
+int [] GetArrayFromString(string text)
+{
+    string []elements = text.Split(" ");
+    int []mass = new int [elements.Length];
+    for (int i=0; i<mass.Length; i++)
+    mass[i] = int.Parse(elements[i]);
+    return mass;
+}
+
+Console.WriteLine ("Введите число:");
+int n = int.Parse(Console.ReadLine());
+
+bool IsIt (int []mass, int n)
+{
+    foreach(var i in mass) //var подстраивает тип данных i по типу данных mass
+    {
+        if (mass[i] == n)
+        return true;
+    }
+    return false;
+}
+
+Console.Write($"{n}, массив [{String.Join(", ", GetArrayFromString(text))}] -> ");
+
+if (IsIt(GetArrayFromString(text), n))
+Console.WriteLine("да");
+else Console.WriteLine("нет");
+
+
+
